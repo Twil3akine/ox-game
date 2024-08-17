542edd3d15e25560037527887194d6705e6d4b00
twil3 <twil3akine@gmail.com>
2024-08-17 15:25:22 +0900

    Complete react_Tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,99 @@
-import React from 'react';
-import Board from './components';
+import styles from "./style.css";
+import { useState } from 'react';
 
-export default function App() {
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
+  for (let i=0; i<lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function Square({ val, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{val}</button>;
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) { return; }
+
+    const nextSquares = squares.slice();
+    nextSquares[i] = (xIsNext) ? "X":"O";
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status = (winner) ? "Winner: " + winner : "Next: " + ((xIsNext===true) ? "X":"O");
+
   return (
-    <>
-      <Board />
-    </>
+    <div className='board'>
+      <div className='status'>{status}</div>
+      <div className='board-row'>
+        <Square val={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square val={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square val={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className='board-row'>
+        <Square val={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square val={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square val={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className='board-row'>
+        <Square val={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square val={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square val={squares[8]} onSquareClick={() => handleClick(8)} />
+
+      </div>
+    </div>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove%2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove+1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length-1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+  const moves = history.map((squares, move) => {
+    let description = (move > 0) ? "Go to move #" + move : "Go to game-start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import Game from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>
 );
